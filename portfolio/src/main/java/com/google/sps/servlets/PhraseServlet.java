868c7cbf9fbdaab9b1b5c49aa99d30b7351a2fe3
Carlos Ortega <carlos.ortega2001@hotmail.com>
2021-03-05 00:27:13 +0000

    Add a servlet that writes a hard-coded string as the response when a user requests its URL.

---
+++ portfolio/src/main/java/com/google/sps/servlets/PhraseServlet.java
@@ -0,0 +1,22 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns HTML that contains the page view count. */
+@WebServlet("/phrase")
+public class PhraseServlet extends HttpServlet {
+
+  private String phrase = "Hello World";
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Phrase</h1>");
+    response.getWriter().println("<p>" + phrase + "</p>");
+  }
+}
